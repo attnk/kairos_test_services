dbb119e1f94183b94a588a347d895941add7a2ca
Unknown <adritanaka@gmail.com>
2018-07-06 18:17:46 -0300

    Incluindo projetos base
    - Incluindo projeto para o serviço de balance brackets
    - incluindo projeto para o serviço de contact list

---
+++ balance-brackets-service/src/main/java/balancebracketsservice/BalanceBracketsServiceApplication.java
@@ -0,0 +1,12 @@
+package balancebracketsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceBracketsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceBracketsServiceApplication.class, args);
+	}
+}
---
+++ contact-list-service/src/main/java/contactlistservice/ContactListServiceApplication.java
@@ -0,0 +1,12 @@
+package contactlistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactListServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactListServiceApplication.class, args);
+	}
+}
