33868d29171e2a914fbba2850b06846cbb29c828
Unknown <adritanaka@gmail.com>
2018-07-09 01:26:40 -0300

    Incluindo Exceptions
    - Incluindo exception específica para as exceptions ligadas aos repositórios
    - incluindo exception específica para as exceptions de validações na service

---
+++ contact-list-service/src/main/java/contactlistservice/exception/RepositoryException.java
@@ -0,0 +1,11 @@
+package contactlistservice.exception;
+
+public class RepositoryException extends Exception {
+	
+	private static final long serialVersionUID = 3112129668411001106L;
+
+	public RepositoryException(String string, Throwable cause) {
+		super(string, cause);
+	}
+
+}
---
+++ contact-list-service/src/main/java/contactlistservice/exception/ServiceException.java
@@ -0,0 +1,11 @@
+package contactlistservice.exception;
+
+public class ServiceException extends Exception {
+
+	private static final long serialVersionUID = 5344770322640173994L;
+
+	public ServiceException(Throwable cause) {
+		super(cause);
+	}
+
+}
